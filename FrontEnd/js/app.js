80c0e693bf5627c02dd80af742d0c83e19be137f
Camila CROSET <potycamila@gmail.com>
2024-10-07 13:55:17 +0200

    Update styles, index, and JavaScript functionalities

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,12 +1,15 @@
 // Variable goblal
-const modalBtnAddPhoto = document.querySelector(".modal-btn-add-photo");
-const modalGalleryView = document.querySelector(".modal-gallery-view");
-const addPhotoView = document.querySelector(".modal-add-photo-view");
 const addPhotoTitle = document.querySelector("#addPhotoTitle");
-const modalAddPhoto = document.querySelector("#modal-add-photo");
 const galerietitle = document.querySelector("#galleryTitle");
 const modalAll = document.querySelector(".modal");
-const backToGalery = document.querySelector(".back-to-gallery");
+const modalBtnAddPhoto = document.querySelector(".modal-btn-add-photo");
+const modalGalleryView = document.querySelector(".modal-gallery-view");
+const modalGallery = document.querySelector("#modal-gallery");
+const addPhotoModal = document.querySelector("#modal-add-photo"); // Parent modal
+const addPhotoView = document.querySelector(".modal-add-photo-view");
+const backToGallery = document.querySelector(".back-to-gallery");
+const jsModal = document.querySelector(".js-modal");
+const loged = window.localStorage.getItem("token") || false;
 
 // Fonction pour ajouter les projets à la galerie
 function addWorksToGallery(works) {
@@ -107,43 +110,99 @@ let modal = null;
 
 const openModal = function (e) {
   e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
-  target.style.display = "flex"; // Annulation du display: none
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
+
+  const targetElement = e.target.closest('a');
+
+  if (!targetElement) return;
+
+  const target = document.querySelector(targetElement.getAttribute("href"));
+
+  if (!target) return;
+
+  if (modal) closeModal();
+
   modal = target;
 
-  if (modal.id === "modal-gallery") {
+  console.log("ouvrir modal", modal.id)
+
+  modal.style.display = "flex";
+  modal.classList.add("is-visible");
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
+
+  if (modal.id === 'modal-gallery') {
     addWorksToModal(worksData);
+    modalGalleryView.style.display = "flex"
   }
-  modal.addEventListener("click", closeModal);
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
-  modal
-    .querySelector(".modal-gallery-view")
-    .addEventListener("click", stopPropagation);
-  document
-    .querySelector(".modal-add-photo-view")
-    .addEventListener("click", stopPropagation);
+
+  const closeButtons = modal.querySelectorAll(".js-modal-close");
+  closeButtons.forEach(button => {
+    if (!button.dataset.listenerAdded) {
+      console.log("Ajoute click au buton x", button);
+      button.addEventListener("click", closeModal);
+      button.dataset.listenerAdded = "true";
+    }
+  });
+
+  modal.addEventListener("click", closeModalOnOutsideClick);
 };
 
 const closeModal = function (e) {
+  if (e) {
+    e.preventDefault(); 
+  }
+
   if (modal === null) return;
-  e.preventDefault();
-  modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
+
+  console.log("Fermeture du modal", modal.id);
+
+  modal.classList.remove("is-visible");
   modal.removeAttribute("aria-modal");
-  modal.removeEventListener("click", closeModal);
-  modal
-    .querySelector(".js-modal-close")
-    .removeEventListener("click", closeModal);
-  modal
-    .querySelector(".modal-gallery-view")
-    .removeEventListener("click", stopPropagation);
-  modal = null;
+  modal.setAttribute('aria-hidden', 'true');
+
+  if (modal.id === 'modal-add-photo') {
+    addPhotoView.style.display = "none";
+    addPhotoModal.style.display = "none";
+  } else if (modal.id === 'modal-gallery') {
+    modalGalleryView.style.display = "none";
+    modalGallery.style.display = "none";
+  }
+
+  const openModalButton = document.querySelector('.js-modal');
+  if (openModalButton && !modal.hasAttribute ('aria-hidden')) {
+      openModalButton.focus();
+  }
+
+  document.querySelectorAll(".modal").forEach(modalElement => {
+    modalElement.classList.remove("is-visible");
+    modalElement.style.display = "none";
+  });
+
+  modal.removeEventListener("click", closeModalOnOutsideClick);
+  modal = null
 };
 
-const stopPropagation = function (e) {
-  e.stopPropagation();
+const closeButtons = document.querySelectorAll(".js-modal-close");
+closeButtons.forEach(button => {
+  button.addEventListener('click', (e) => { 
+      console.log("Bouton de fermeture cliqué");
+      closeModal(e);
+  });
+});
+
+const closeModalOnOutsideClick = function (e) {
+  if (e.target === modal || e.target.classList.contains('modal')) {
+    if (modal.id === 'modal-add-photo') {
+      addPhotoView.classList.remove("is-visible");
+      addPhotoModal.classList.remove("is-visible")
+      addPhotoModal.style.display = "none";
+    } else if (modal.id === 'modal-gallery') {
+      modalGalleryView.classList.remove("is-visible");
+      modalGallery.classList.remove("is-visible");
+      modalGallery.style.display = "none";
+    }
+    closeModal(e);
+  }
 };
 
 document.querySelectorAll(".js-modal").forEach((a) => {
@@ -209,25 +268,85 @@ function deleteWork(workId, figureElement) {
     .catch((error) => console.error("Error:", error));
 }
 
-// Modal - Formulaire
-  modalBtnAddPhoto.addEventListener("click", function() {
-    modalGalleryView.style.display = "none";
+// Gestion de l'événement du clic sur "Ajouter une photo"
+// Fonction pour basculer entre les modales
+
+// Sélection des éléments des modales
+
+// Fonction pour basculer entre les modales
+function toggleModals() {
+  // Vérifier si les éléments existent
+  if (
+    !modalBtnAddPhoto ||
+    !modalGalleryView ||
+    !addPhotoModal ||
+    !addPhotoView ||
+    !backToGallery
+  ) {
+    console.error("Un ou plusieurs éléments DOM ne sont pas trouvés");
+    return;
+  }
+
+  // Gestion de l'événement du clic sur "Ajouter une photo"
+  modalBtnAddPhoto.addEventListener("click", function () {
+    console.log("Bouton Ajouter une photo cliqué");
+
+    // Masquer la galerie et afficher la modale d'ajout de photo
+    modalGalleryView.classList.remove("is-visible");
+    modalGallery.classList.remove("is-visible");
+    addPhotoModal.style.display = "flex";
     addPhotoView.style.display = "flex";
-    addPhotoTitle.textContent = "Ajout photo";
-});
+    addPhotoModal.classList.add("is-visible");
+    addPhotoView.classList.add("is-visible");
+  });
 
-backToGalery.addEventListener("click", function () {
-  modalGalleryView.style.display = "flex";
-  addPhotoView.style.display = "none";
-  addPhotoTitle.textContent = "Galerie photo";
-});
+  // Gestion du retour à la galerie
+  backToGallery.addEventListener("click", function () {
+    console.log("Bouton Retour à la galerie cliqué");
 
+    // Masquer la vue ajout de photo et afficher la galerie
+    addPhotoView.classList.remove("is-visible");
+    addPhotoModal.classList.remove("is-visible");
+    addPhotoModal.style.display = "none";
+    modalGalleryView.classList.add("is-visible");
+    modalGallery.classList.add("is-visible");
+  });
+
+  addPhotoModal.addEventListener("click", function (e) {
+    if (e.target === addPhotoModal || e.target.classList.contains ("js-modal-close")) {
+      addPhotoView.classList.remove("is-visible");
+      addPhotoModal.classList.remove("is-visible");
+      addPhotoModal.style.display = "none";
+    }
+  });
+
+  modalGallery.addEventListener("click", function (e) {
+    if (e.target === modalGallery || e.target.classList.contains("js-modal-close")) {
+      modalGalleryView.classList.remove("is-visible");
+      modalGallery.classList.remove("is-visible");
+      modalGallery.style.display = "none";
+    }
+  });
+}
+
+// Appel de la fonction pour initialiser les événements
+toggleModals();
+
+// Gestion du formulaire de soumission
 document
   .getElementById("add-photo-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
 
     const formData = new FormData(this);
+
+    console.log(localStorage.getItem("token"));
+
+    if (!localStorage.getItem("token")) {
+      console.error("Token non trouvé, utilisateur non connecté.");
+      return;
+    }
+
     fetch("http://localhost:5678/api/works", {
       method: "POST",
       headers: {
@@ -235,13 +354,32 @@ document
       },
       body: formData,
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
+        console.log("Success: data", data);
         addNewWorkToGallery(data);
-        document.querySelector(".modal-gallery-view").style.display = "block";
-        document.querySelector(".modal-add-photo-view").style.display = "none";
-        document.querySelector("#titlemodal").textContent = "Galerie photo";
-        this.reset();
+        
+        modalGalleryView.classList.add("is-visible");
+        addPhotoView.style.display = "none";
+        addPhotoTitle.textContent = "Galerie photo";
+        addPhotoModal.classList.remove("is-visible");
+
+        modalGallery.classList.add("is-visible");
+
+        modal.classList.add("modal");
+
+        document.getElementById("add-photo-form").reset();
+
+        const preview = document.getElementById("image-preview2");
+        const uploadContent = document.querySelector(".uploas-content");
+        preview.src = "";
+        preview.style.display = "none";
+        uploadContent.style.display ="flex"
       })
       .catch((error) => console.error("Erreur:", error));
   });
@@ -285,12 +423,14 @@ populateCategories();
 // previsualisation de l'image
 document.getElementById("image").addEventListener("change", function (event) {
   const file = event.target.files[0];
-  const preview = document.getElementById("image-preview");
+  const preview = document.getElementById("image-preview2");
+  const uploadContent = document.querySelector(".upload-content");
   const reader = new FileReader();
   if (file) {
     reader.onload = function (e) {
       preview.src = e.target.result;
-      document.querySelector(".image-preview").style.display = "flex";
+      preview.style.display = "flex";
+      uploadContent.style.display = "none";
     };
     reader.readAsDataURL(file);
   }
@@ -299,19 +439,25 @@ document.getElementById("image").addEventListener("change", function (event) {
 // Logout
 document.addEventListener('DOMContentLoaded', function () {
   const token = localStorage.getItem('token');
-  const loginLink = document.querySelector('nav ul li a[href="login.html"]');
+  const portfolioSection = document.querySelector('#portfolio');
+  const editBox = document.querySelector('.edit-box');
 
   if (token) {
-    document.querySelector('.edit-box').style.display = 'flex';
-
+    portfolioSection.classList.add('logged-in')
+  
+    const loginLink = document.querySelector('nav ul li a[href="login.html"]');
     loginLink.textContent = 'logout'
     loginLink.href = '#';
     loginLink.classList.add('logout-link');
+
     loginLink.addEventListener('click', function () {
       // Deconnexion
       localStorage.removeItem('token');
       window.location.href = 'index.html';
     });
+  } else {
+    portfolioSection.classList.remove('logged-in');
+    editBox.style.display = 'none';
   }
 });
 
